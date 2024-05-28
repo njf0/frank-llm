3ceacc535d0f45e30b9a2d87e5b7911560e29dc6
Nick Ferguson <39011127+njf0@users.noreply.github.com>
2024-05-28 16:51:09 +0100

    Delete access-tokens.py (token now invalidated)

--- access-tokens.py
+++
@@ -1,8 +0,0 @@
-from huggingface_hub import HfApi, HfFolder
-
-hf_access_token = "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"
-
-HfFolder.save_token(hf_access_token)
-api = HfApi()
-user = api.whoami(hf_access_token)
-print(user)
