659d0de90f463ef91b29175cd8301382a828cba3
njf0 <goggled.mapping.0p@icloud.com>
2024-08-12 13:54:54 +0000

    Update setup.sh with remote installation of VS Code extensions

--- setup.sh
+++ setup.sh
@@ -1,27 +1,22 @@
-code --install-extension github.copilot
-code --install-extension github.copilot-chat
-code --install-extension mechatroner.rainbow-csv
-code --install-extension ms-azuretools.vscode-docker
-code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
-code --install-extension ms-python.autopep8
-code --install-extension ms-python.black-formatter
-code --install-extension ms-python.debugpy
-code --install-extension ms-python.isort
-code --install-extension ms-python.pylint
-code --install-extension ms-python.python
-code --install-extension ms-python.vscode-pylance
-code --install-extension ms-toolsai.jupyter
-code --install-extension ms-toolsai.jupyter-keymap
-code --install-extension ms-toolsai.jupyter-renderers
-code --install-extension ms-toolsai.vscode-jupyter-cell-tags
-code --install-extension ms-toolsai.vscode-jupyter-slideshow
-code --install-extension ms-vscode-remote.remote-ssh
-code --install-extension ms-vscode-remote.remote-ssh-edit
-code --install-extension ms-vscode-remote.remote-wsl
-code --install-extension ms-vscode.remote-explorer
-code --install-extension ms-vscode.remote-server
-code --install-extension pkief.material-icon-theme
-code --install-extension redhat.vscode-yaml
+code --install-extension "github.copilot" --remote $HOSTNAME
+code --install-extension "github.copilot-chat" --remote $HOSTNAME
+code --install-extension "mechatroner.rainbow-csv" --remote $HOSTNAME
+code --install-extension "ms-azuretools.vscode-docker" --remote $HOSTNAME
+code --install-extension "ms-kubernetes-tools.vscode-kubernetes-tools" --remote $HOSTNAME
+code --install-extension "ms-python.autopep8" --remote $HOSTNAME
+code --install-extension "ms-python.debugpy" --remote $HOSTNAME
+code --install-extension "ms-python.isort" --remote $HOSTNAME
+code --install-extension "ms-python.pylint" --remote $HOSTNAME
+code --install-extension "ms-python.python" --remote $HOSTNAME
+code --install-extension "ms-python.vscode-pylance" --remote $HOSTNAME
+code --install-extension "ms-toolsai.jupyter" --remote $HOSTNAME
+code --install-extension "ms-toolsai.jupyter-keymap" --remote $HOSTNAME
+code --install-extension "ms-toolsai.jupyter-renderers" --remote $HOSTNAME
+code --install-extension "ms-toolsai.vscode-jupyter-cell-tags" --remote $HOSTNAME
+code --install-extension "ms-toolsai.vscode-jupyter-slideshow" --remote $HOSTNAME
+code --install-extension "pkief.material-icon-theme" --remote $HOSTNAME
+code --install-extension "redhat.vscode-yaml" --remote $HOSTNAME
+code --install-extension "tecosaur.latex-utilities" --remote $HOSTNAME
 pip3 install --upgrade pip
 pip3 install --upgrade git+https://www.github.com/frank-lab-ai/franky@njf
 pip3 install --upgrade accelerate
